17b27be9c14ae91495020a0f1289f1dac10065a4
Gurk117 <guriguri17884@gmail.com>
2025-10-01 13:21:01 +0530

    new html

--- vs code/python/1.py
+++ vs code/python/1.py
@@ -17,12 +17,29 @@
 #         c += 1
 # print(c)
 
-a = input()
-rev = ""
-for char in a:
-    rev = char + rev
-if a == rev:
-    print("palindrome")
-else:
-    print("not palindrome")
-    
+# a = input()
+# rev = ""
+# for char in a:
+#     rev = char + rev
+# if a == rev:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+#print all the characters until you find a digit
+# a = input()
+# for char in a:
+#     if char.isdigit():
+#         break
+#     print(char)
+
+#print all the characters until you find a special character
+# a  = input()
+# for char in a:
+#     if char.isalpha():
+#         print(char)
+#     elif char.isdigit():
+#         print(char)
+#     else:
+#         print()
+#take
